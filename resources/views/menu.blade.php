aaa14a157b3883c89c179b04ed40b51a1afe5338
Manh Hung <hung.nxm194292@sis.hust.edu.vn>
2022-11-07 16:46:37 +0700

    fix js

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,71 +1,23 @@
 <div id="menu-expand" class="menu-expand-wrapper">
     <div class="canle">
         <ul class="menu-expand-list">
+            @if(isset($category))
+            @foreach($category as $item)
             <li class="menu-expand-item">
                 <ul class="sub-menu-expand-list">
                     <li class="sub-menu-expand-item cate">
-                        <a href="#">Mobile</a>
+                        <a href="{{ route('view.cate', ['id' => $item->id]) }}">{{ $item->name }}</a>
                     </li>
-                    <li class="sub-menu-expand-item">
-                        <a href="#">Điện thoại</a>
-                    </li>
-                    <li class="sub-menu-expand-item">
-                        <a href="#">Máy tính bảng</a>
-                    </li>
+{{--                    <li class="sub-menu-expand-item">--}}
+{{--                        <a href="#">Điện thoại</a>--}}
+{{--                    </li>--}}
+{{--                    <li class="sub-menu-expand-item">--}}
+{{--                        <a href="#">Máy tính bảng</a>--}}
+{{--                    </li>--}}
                 </ul>
             </li>
-            <li class="menu-expand-item">
-                <ul class="sub-menu-expand-list">
-                    <li class="sub-menu-expand-item cate">
-                        <a href="#">Internet</a>
-                    </li>
-                    <li class="sub-menu-expand-item">
-                        <a href="#">Digital Marketing</a>
-                    </li>
-                    <li class="sub-menu-expand-item">
-                        <a href="#">Media</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="menu-expand-item">
-                <ul class="sub-menu-expand-list">
-                    <li class="sub-menu-expand-item cate">
-                        <a href="#">Khám phá</a>
-                    </li>
-                    <li class="sub-menu-expand-item">
-                        <a href="#">Lịch sử</a>
-                    </li>
-                    <li class="sub-menu-expand-item">
-                        <a href="#">Tri thức</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="menu-expand-item">
-                <ul class="sub-menu-expand-list">
-                    <li class="sub-menu-expand-item cate">
-                        <a href="#">Tin ICT</a>
-                    </li>
-                    <li class="sub-menu-expand-item">
-                        <a href="#"></a>
-                    </li>
-                    <li class="sub-menu-expand-item">
-                        <a href="#"></a>
-                    </li>
-                </ul>
-            </li>
-            <li class="menu-expand-item">
-                <ul class="sub-menu-expand-list">
-                    <li class="sub-menu-expand-item cate">
-                        <a href="#">Mobile</a>
-                    </li>
-                    <li class="sub-menu-expand-item">
-                        <a href="#">Điện thoại</a>
-                    </li>
-                    <li class="sub-menu-expand-item">
-                        <a href="#">Máy tính bảng</a>
-                    </li>
-                </ul>
-            </li>
+            @endforeach
+            @endif
         </ul>
     </div>
 </div>
